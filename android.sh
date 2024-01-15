2d4f1d3bb791497e7db9bf4e61cb7299057eb942
luboslenco <lubos.lenco@gmail.com>
2024-01-15 10:56:49 +0100

    Init

---
+++ android.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/facebook/hermes -b static_h
+cd hermes
+git checkout 5713c8a
+cd ..
---
+++ windows_debug.bat
@@ -0,0 +1,7 @@
+git clone https://github.com/facebook/hermes -b static_h
+cd hermes
+git checkout 5713c8a
+cd ..
+
+cmake -S hermes -B build -G 'Visual Studio 17 2022' -T ClangCL -A x64 -DCMAKE_BUILD_TYPE=Debug -DHERMES_UNICODE_LITE=1 -DHERMES_ENABLE_TEST_SUITE=0
+cmake --build ./build --config Debug
