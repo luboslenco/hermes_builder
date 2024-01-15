b7eca9742f96a4cba460a3fe55d5e52c434b2511
luboslenco <lubos.lenco@gmail.com>
2024-01-15 12:46:40 +0100

    Init

--- android.sh
+++ android.sh
@@ -2,3 +2,9 @@ git clone https://github.com/facebook/hermes -b static_h
 cd hermes
 git checkout 5713c8a
 cd ..
+
+cmake -S hermes -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DHERMES_ENABLE_TEST_SUITE=0
+cmake --build ./build
+
+cd hermes/android
+./gradlew githubRelease
--- ios.sh
+++ ios.sh
@@ -2,3 +2,9 @@ git clone https://github.com/facebook/hermes -b static_h
 cd hermes
 git checkout 5713c8a
 cd ..
+
+cmake -S hermes -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DHERMES_UNICODE_LITE=1 -DHERMES_ENABLE_TEST_SUITE=0
+cmake --build ./build
+
+cd hermes
+./utils/build-ios-framework.sh
